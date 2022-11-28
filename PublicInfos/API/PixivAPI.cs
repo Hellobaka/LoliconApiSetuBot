98ef8da0c80b0265b9c17ae29b68fe10411a5c9b
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-11-28 17:45:43 +0800

    移植本地PixivAPI

--- PublicInfos/API/PixivAPI.cs
+++ PublicInfos/API/PixivAPI.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using Scighost.PixivApi.Illust;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace me.cqp.luohuaming.Setu.PublicInfos.API
 {
     public static class PixivAPI
     {
-        public static string GetPicInfo(int id)
+        public static IllustInfo GetPicInfo(int id)
         {
+            var task = MainSave.PixivClient.GetIllustInfoAsync(id);
+            IllustInfo info = task.Result;
+            task.Wait();
+            return info;
+        }
 
+        public static string DownloadPic(int id, string path)
+        {
+            IllustInfo info = GetPicInfo(id);
+            var downloadTask = MainSave.PixivClient.DownloadPic(info, path);
+            string filename = downloadTask.Result;
+            downloadTask.Wait();
+            return filename;
+        }
+
+        public static List<IllustRankItem> GetRankList(RankType type)
+        {
+            var task = MainSave.PixivClient.GetRankingList(type);
+            var result = task.Result;
+            task.Wait();
+            return result;
         }
     }
 }
